613efbcd05dd973a4105c919ca1ead32ce0bf224
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-17 07:25:14 +0700

    + employee auth controller

---
+++ app/Http/Controllers/EmployeeAuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeAuthController extends Controller
+{
+    public function login()
+    {
+        $input = request()->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (auth('employee')->attempt($input)) {
+            request()->session()->regenerate();
+            return redirect('/employee');
+        }
+
+        return redirect('/employee/login')->withErrors(['username' => 'username atau password salah']);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/employee/login');
+    }
+}
