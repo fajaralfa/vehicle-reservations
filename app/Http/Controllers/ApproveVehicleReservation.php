7db2378df57e5183de95167140fcac9d53f5d3d3
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-17 16:08:34 +0700

    + approve controller

--- app/Http/Controllers/ApproveVehicleReservation.php
+++ app/Http/Controllers/ApproveVehicleReservation.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\VehicleReservation;
 use Illuminate\Http\Request;
 
 class ApproveVehicleReservation extends Controller
 {
-    //
+    public function index()
+    {
+        $vehicle_approvals = VehicleReservation::
+            where('approver_id', auth('employee')->user()->id)
+            ->latest()
+            ->orderBy('is_approved')
+            ->get();
+
+        return view('employee.vehicle-approval.index', [
+            'vehicle_approvals' => $vehicle_approvals,
+        ]);
+    }
+
+    public function show(string $id)
+    {
+        $vehicleReservation = VehicleReservation::
+            where('approver_id', auth('employee')->user()->id)
+            ->where('id', $id)
+            ->first();
+
+        return view('employee.vehicle-approval.show-approval', [
+            'vehicleReservation' => $vehicleReservation,
+        ]);
+    }
+
+    public function approve(string $id)
+    {
+        $answer = request()->post('answer');
+
+        $vehicleReservation = VehicleReservation::
+            where('approver_id', auth('employee')->user()->id)
+            ->where('id', $id)
+            ->first();
+
+        $vehicleReservation->is_approved = $answer;
+        $vehicleReservation->approved_date = now();
+        $vehicleReservation->save();
+
+        return redirect("/employee/vehicle-approval/$id");
+    }
 }
