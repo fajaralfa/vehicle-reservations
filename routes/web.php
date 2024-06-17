8d6914186d03fbb507348df817e48c17f07863f2
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-17 16:11:20 +0700

    + add all routes to

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\ApproveVehicleReservation;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmployeeAuthController;
+use App\Http\Controllers\EmployeeDashboardController;
+use App\Http\Controllers\RequestVehicleReservation;
+use App\Http\Controllers\VehicleDriverResourceController;
+use App\Http\Controllers\ProcessVehicleReservationController;
+use App\Http\Controllers\VehicleResourceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+
+Route::prefix('admin')->group(function () {
+    Route::view('/login', 'admin.login');
+    Route::post('/login', [AdminAuthController::class, 'login']);
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::get('/', DashboardController::class);
+        Route::post('/logout', [AdminAuthController::class, 'logout']);
+
+        Route::get('/vehicle-reservations', [ProcessVehicleReservationController::class, 'index']);
+        Route::get('/vehicle-reservations/{id}', [ProcessVehicleReservationController::class, 'show']);
+        Route::get('/vehicle-reservations/{id}/process', [ProcessVehicleReservationController::class, 'processPage']);
+        Route::post('/vehicle-reservations/{id}/process', [ProcessVehicleReservationController::class, 'process']);
+
+        Route::resource('vehicles', VehicleResourceController::class);
+        Route::resource('vehicle-drivers', VehicleDriverResourceController::class);
+    });
+});
+
+Route::prefix('employee')->group(function () {
+    Route::view('/login', 'employee.login')->name('login');
+    Route::post('/login', [EmployeeAuthController::class, 'login']);
+    Route::post('/logout', [EmployeeAuthController::class, 'logout']);
+    Route::middleware(['auth:employee'])->group(function () {
+        Route::get('/', EmployeeDashboardController::class);
+        Route::resource('vehicle-reservations', RequestVehicleReservation::class)
+            ->only('index', 'show', 'create', 'store', 'destroy');
+        Route::get('vehicle-approval', [ApproveVehicleReservation::class, 'index']);
+        Route::get('vehicle-approval/{id}', [ApproveVehicleReservation::class, 'show']);
+        Route::post('vehicle-approval/{id}/approve', [ApproveVehicleReservation::class, 'approve']);
+    });
 });
